c1a1d791926e12332b1fd11dd84d70b05d487045
Alexander V. Ribchansky <avr39ripe@gmail.com>
2016-10-12 18:07:12 +0300

    Add userSTAGotIP ovveride to AppClass

--- include/app.h
+++ include/app.h
@@ -14,6 +14,7 @@ class AppClass : public ApplicationClass
 public:
 	virtual void init(); // Application initialization
 	virtual void start(); // Application main-loop start/restart
+	virtual void userSTAGotIP(IPAddress ip, IPAddress mask, IPAddress gateway); // Runs when Station got ip from access-point
 protected:
 	virtual void _loop(); // Application main loop function goes here
 };
