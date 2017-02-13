e95e034214ec373cde634354361f5adc8be94956
Alexander V. Ribchansky <avr39ripe@gmail.com>
2017-02-13 08:51:45 +0200

    Add mimification support

--- files/webpack.config.js
+++ files/webpack.config.js
@@ -1,9 +1,28 @@
 const webpack = require('webpack');
+const BabiliPlugin = require("babili-webpack-plugin");
 
 module.exports = {
   entry: './index_new.js',
   output: {
     filename: 'bundle.js'
   },
-  devtool: 'source-map'
+  module: {
+  loaders: [
+    {
+      test: /\.js$/,
+      exclude: /(node_modules|bower_components)/,
+      loader: 'babel-loader',
+      query: {
+        presets: ['es2015']
+      }
+    }
+  ]
+},
+  devtool: 'source-map',
+  plugins: [
+//    new webpack.optimize.UglifyJsPlugin({
+//      compress: { warnings: false }
+//    })
+  new BabiliPlugin()
+  ] 
 }
