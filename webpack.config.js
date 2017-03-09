277a86d81ddfc48d6632eddc54e216a61de24ad8
Alexander V. Ribchansky <avr39ripe@gmail.com>
2017-03-09 19:09:37 +0200

    Build index_new.html by webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,18 @@
 const path = require('path');
 const webpack = require('webpack');
 const BabiliPlugin = require("babili-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
 	resolve: {
 		modules: [path.resolve(__dirname, "lib/application"), path.resolve(__dirname, "lib/binio"), "node_modules"]
 	},
-	entry : './files/index_new.js',
+	entry : {
+		index: './files/index_new.js'
+	},
 	output : {
-		filename : './files/bundle.js'
+		path: path.join(__dirname, 'files'),
+		filename : '[name].[hash].js'
 	},
 	module : {
 		loaders : [ {
@@ -25,5 +29,11 @@ module.exports = {
 	//    new webpack.optimize.UglifyJsPlugin({
 	//      compress: { warnings: false }
 	//    })
-	new BabiliPlugin() ]
+	new BabiliPlugin(),
+	new HtmlWebpackPlugin({
+		template: path.resolve('./', './files/index-template.html'),
+		filename: path.resolve('./', './files/index_new.html'),
+		inject: 'head'
+	    })
+	]
 }
