f9ffcb845eb2921bc2d7a1e053f018ebec13818f
Alexander V. Ribchansky <avr39ripe@gmail.com>
2019-11-04 16:12:42 +0200

    Add file to name httpBinState* entities

---
+++ files/BinStatesNames.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const BinStatesNames = {
+	"states" : {
+		"0" : "Зона 1",
+		"1" : "Зона 2",
+		"2" : "Зона 3",
+		"3" : "Газовый котел"
+	},
+	"buttons" : {
+		"0" : "Старт",
+		"1" : "Стоп",
+		"2" : "Газовое отопление",
+	}
+};
+const BinStatesGroups = [
+]
+
+export { BinStatesNames, BinStatesGroups };
