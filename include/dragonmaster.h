605e02afc70694b6b869d238b485d65045488821
Alexander V. Ribchansky <avr39ripe@gmail.com>
2019-11-16 15:36:26 +0200

    Add needed includes and constants for Gabor project

--- include/dragonmaster.h
+++ include/dragonmaster.h
@@ -8,10 +8,16 @@
 #pragma once
 #include <SmingCore.h>
 #include <Libraries/OneWire/OneWire.h>
+#include <JsonObjectStream.h>
 #include <tempsensors.h>
 #include <binin.h>
 #include <binout.h>
 #include <binhttpbutton.h>
 #include <thermostat.h>
+#include <weekthermostat.h>
 
+const uint8_t zonesCount = 5; // Zones quantity
+const uint8_t caldronOnDelay = 1; //Caldron ON delay in minutes
+extern BinOutClass* outputs[8];
 extern TempSensorsHttp* tempSensorsHttp;
+extern WeekThermostatClass* weekThermostats[zonesCount];
