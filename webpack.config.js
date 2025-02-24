f040e0ca2bcc2dc7bd6fa077376d620eae08b4c0
avr39-ripe <avr39ripe@gmail.com>
2025-02-24 16:38:19 +0000

    Upgrade webpack.config.js to fit modern webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,94 +1,89 @@
 const path = require('path');
-const webpack = require('webpack');
-const BabiliPlugin = require("babili-webpack-plugin");
+const TerserPlugin = require('terser-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CompressionPlugin = require('compression-webpack-plugin');
 
 module.exports = {
-	mode : 'development',
-	resolve : {
-		modules : [
-				path.resolve(__dirname, "lib/application"),
-				path.resolve(__dirname, "lib/binio"),
-				path.resolve(__dirname, "lib/tempsensor"),
-				path.resolve(__dirname, "files"), "node_modules"
-		]
-	},
-	entry : {
-		index : './files/index.js',
-		thermostat : './files/thermostat.js'
-	},
-	output : {
-		path : path.join(__dirname, 'files'),
-		filename : '[name].[hash:10].js'
-	},
-	module : {
-		rules : [ {
-			test : /\.(js|jsx)$/,
-			exclude : /node_modules/, // This may not be needed since we supplied `include`.
-			include : path.resolve(__dirname, 'src'),
+    mode: 'development',
 
-			/*
-			  https://goo.gl/99S6sU
-			  Loaders will be applied from right to left.
-			  E.x.: loader3(loader2(loader1(data)))
-			 */
-			use : [
-			// https://goo.gl/EXjzoG
-			{
-				loader : 'babel-loader',
-				options : {
-					presets : [
-					/*
-					  To get tree shaking working, we need the `modules: false` below.
-					  https://goo.gl/4vZBSr - 2ality blog mentions that the issue is caused
-					  by under-the-hood usage of `transform-es2015-modules-commonjs`.
-					  https://goo.gl/sBmiwZ - A comment on the above post shows that we
-					  can use `modules: false`.
-					  https://goo.gl/aAxYAq - `babel-preset-env` documentation.
-					 */
-					[ '@babel/preset-env', {
-						targets : {
-							browsers : [ 'last 2 versions' ]
-						},
-						modules : false
-					// Needed for tree shaking to work.
-					} ], '@babel/preset-env', // https://goo.gl/aAxYAq
-					//'@babel/preset-react' // https://goo.gl/4aEFV3
-					],
+    resolve: {
+        modules: [
+            path.resolve(__dirname, "lib/application"),
+            path.resolve(__dirname, "lib/binio"),
+            path.resolve(__dirname, "lib/tempsensor"),
+            path.resolve(__dirname, "files"),
+            "node_modules"
+        ]
+    },
 
-					// https://goo.gl/N9gaqc - List of Babel plugins.
-					plugins : [
-					//          '@babel/plugin-proposal-object-rest-spread', // https://goo.gl/LCHWnP
-					//          '@babel/plugin-proposal-class-properties' // https://goo.gl/TE6TyG
-					]
-				}
-			} ]
-		}
+    entry: {
+        index: './files/index.js',
+        thermostat: './files/thermostat.js'
+    },
 
-		]
-	},
-	devtool : 'source-map',
-	plugins : [
-	new BabiliPlugin(),
-	new HtmlWebpackPlugin({
-		inject: false,
-		chunks: ['index'],
-//template : path.resolve('./', './files/index-template.html'),
-		template : './files/index-template.html',
-//		filename : path.resolve('./', './files/index_new.html'),
-		inject : 'head'
-	}),
-	new HtmlWebpackPlugin({
-		inject: false,
-		chunks: ['thermostat'],
-		template : './files/thermostat-template.html',
-//		filename: './files/thermostat.html',
-		filename : path.resolve('./', './files/thermostat.html'),
-		inject : 'body'
-	}),
-	new CompressionPlugin({
-		deleteOriginalAssets : true
-	})
-	]
-}
+    output: {
+        path: path.join(__dirname, 'files'),
+        filename: '[name].[contenthash:10].js'  // Using contenthash for caching
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                include: path.resolve(__dirname, 'src'),
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            [
+                                '@babel/preset-env',
+                                {
+                                    targets: {
+                                        browsers: ['last 2 versions']
+                                    },
+                                    modules: false  // Enables tree-shaking
+                                }
+                            ]
+                        ],
+                        plugins: [
+                            // Additional Babel plugins can be added here.
+                        ]
+                    }
+                }
+            }
+        ]
+    },
+
+    devtool: 'source-map',
+
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new TerserPlugin({
+                extractComments: false,
+                terserOptions: {
+                    compress: true,
+                    mangle: true
+                }
+            })
+        ]
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './files/index-template.html',
+            inject: 'head',
+            chunks: ['index']
+        }),
+        new HtmlWebpackPlugin({
+            template: './files/thermostat-template.html',
+            filename: path.resolve('./', './files/thermostat.html'),
+            inject: 'body',
+            chunks: ['thermostat']
+        }),
+        new CompressionPlugin({
+            deleteOriginalAssets: true
+        })
+    ]
+};
